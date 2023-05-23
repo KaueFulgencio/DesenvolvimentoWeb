798b90e2c410deb396d8291a196a3fe56aaa4550
KaueFulgencio <Kauemfulgencio@outlook.com>
2023-05-23 11:07:04 -0300

    Criando serviço pra atualizar um produto

--- api-vendas/src/shared/http/server.ts
+++ api-vendas/src/shared/http/server.ts
@@ -1,15 +1,29 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
+import { pagination } from 'typeorm-pagination';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
+import uploadConfig from '@config/upload';
+import rateLimiter from '@shared/http/middlewares/rateLimiter';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(rateLimiter);
+
+app.use(pagination);
+
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
@@ -19,6 +33,8 @@ app.use(
         message: error.message,
       });
     }
+
+    console.log(error);
 
     return response.status(500).json({
       status: 'error',
